c70057788f1d55e54927691acd6f313092673b06
lukaszflorek <lukaszflorek@gmail.com>
2017-08-29 23:14:47 +0200

    * Reading temperature value from text file * Text file added

--- Temperature/src/TemperatureMain.java
+++ Temperature/src/TemperatureMain.java
@@ -1,5 +1,17 @@
-public class TemperatureMain {
-    public static void main(String args[]) {
+/*
+The temperature value is reading from temp_read text file and then printed to std_out.
+ */
 
+import static java.lang.System.out;
+import java.io.*;
+import java.util.Scanner;
+
+public class TemperatureMain {
+    public static void main(String args[]) throws FileNotFoundException {
+        File tempReadings = new File("Temperature/temp_read");
+        Temperature temp = new Temperature();
+        Scanner seeker = new Scanner(tempReadings);
+        temp.setNumber(seeker.nextDouble());
+        out.println("The temperature is: " + temp.getNumber() + " " + temp.getScale());
     }
 }
